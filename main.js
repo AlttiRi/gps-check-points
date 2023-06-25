7118143625fa93bfab3eb5a3ff0d70a4a9dfb290
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-06-26 02:09:07 +0300

    Update

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ function main() {
     navigator.geolocation.getCurrentPosition(function(location) {
         const obj = {};
         for (const key in location.coords) {
-            obj[key] = location.coords[key]
+            if (location.coords[key] === null) {
+                continue;
+            }
+            obj[key] = location.coords[key];
         }
         console.log(location.coords);
         document.querySelector(".main").textContent = JSON.stringify(obj, null, " ");
