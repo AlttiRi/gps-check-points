c2e8d83bfbf0233287d932b18cf58105334f6fbf
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2023-06-26 14:45:05 +0300

    Update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -27,9 +27,12 @@ function objectify<T>(target: T): T {
     return obj as T;
 }
 
+const i = ref(0);
 async function onClick() {
     try {
+        i.value++;
         coordObj.value = await getCoords();
+        i.value++;
     } catch (e) {
         alert(JSON.stringify(objectify(e as GeolocationPositionError), null, "  "));
     }
@@ -43,6 +46,7 @@ function isObject(target: any): target is object {
 
 <template>
     <div class="main">
+        <div>{{ i }}</div>
         <button @click="onClick">Update Point</button>
         <table class="coord" v-if="coordObj">
             <tr v-for="[k, v] of Object.entries(objectify(coordObj.coords)).filter(([_k, _v]) => _v)">
